37558a705ac05c0f3a58824bd87603740a0401bc
ksu_vers <ksulivigo@gmail.com>
2021-10-13 15:14:26 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -12,12 +12,32 @@ var swiper = new Swiper(".cont-slider", {
         nextEl: '.btn_next',
         prevEl: '.btn_prev',
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        500: {
+
+            slidesPerView: 2
+        },
+        700: {
+            slidesPerView: 3
+        },
+        991: {
+            slidesPerView: 4
+        },
+        1200: {
+            slidesPerView: 5
+        }
+
+    },
 });
 var swiper = new Swiper(".slider_text", {
     slidesPerView: 1,
-    loop: true,
+
     observer: true,
     observeParents: true,
+    spaceBetween: 35,
     navigation: {
         nextEl: '.btn_next',
         prevEl: '.btn_prev',
